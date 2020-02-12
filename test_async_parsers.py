c9c2f1c2ae0ce15d7acbcada4bd7ffab3df54048
eignnx <eignnx@gmail.com>
2020-02-12 17:21:26 -0500

    refactor tests

--- test_async_parsers.py
+++ test_async_parsers.py
@@ -20,6 +20,35 @@ from async_parsers import (
 
 T = TypeVar("T")
 
+errors_so_far = 0
+
+
+def test(parser, input, expected, print_failure=False):
+    try:
+        actual = run_parser(parser, input)
+        if expected is Failure:
+            assert actual.failed
+            if print_failure:
+                print(
+                    f"GOOD PARSE FAILURE ON run_parser({parser}, {input!r}) BECAUSE ```"
+                )
+                print(actual)
+                print("```")
+        elif expected is Success:
+            assert actual.succeeded
+        else:
+            assert actual == expected
+    except AssertionError:
+        global errors_so_far
+        errors_so_far += 1
+        print()
+        print(f"~~~~ASSERTION ERROR #{errors_so_far}~~~~")
+        print(f"\trun_parser({parser}, {input!r}) != {expected}")
+        print(f"\tPARSE RESULT:```")
+        print(actual)
+        print("```")
+        print()
+
 
 @parser_factory
 async def list_of_xs() -> List[str]:
@@ -30,10 +59,9 @@ async def list_of_xs() -> List[str]:
 
 
 p = list_of_xs()
-assert run_parser(p, "[X]") == Success(["X"])
-assert run_parser(p, "[X, X, X]") == Success(["X", "X", "X"])
-assert run_parser(p, "[X, Z, X]").failed
-print(run_parser(p, "[X, Z, X]"))
+test(p, "[X]", Success(["X"]))
+test(p, "[X, X, X]", Success(["X", "X", "X"]))
+test(p, "[X, Z, X]", Failure)
 
 
 @parser_factory
@@ -44,14 +72,12 @@ async def n_bangs():
     return count
 
 
-assert run_parser(n_bangs(), "12!!!!!!!!!!!!") == Success(12)
-assert run_parser(n_bangs(), "3!").failed
-print(run_parser(n_bangs(), "3!"))
+test(n_bangs(), "12!!!!!!!!!!!!", Success(12))
+test(n_bangs(), "3!", Failure)
 
 
-assert run_parser(py_int(), "123").succeeded
-assert run_parser(py_int(), "asdf").failed
-print(run_parser(py_int(), "asdf"))
+test(py_int(), "123", Success)
+test(py_int(), "asdf", Failure)
 
 
 A = TypeVar("A")
@@ -80,19 +106,16 @@ pair_parser = delimited(
     exactly("("), separated_pair(py_int(), exactly(", "), py_int()), exactly(")"),
 )
 
-assert run_parser(pair_parser, "(1, 2)") == Success((1, 2))
-assert run_parser(pair_parser, "(1, 2").failed
-print(run_parser(pair_parser, "(1, 2"))
-assert run_parser(pair_parser, "(1, x)").failed
-print(run_parser(pair_parser, "(1, x)"))
+test(pair_parser, "(1, 2)", Success((1, 2)))
+test(pair_parser, "(1, 2", Failure)
+test(pair_parser, "(1, x)", Failure)
 
 
 foo_bar_or_baz = either(exactly("foo"), exactly("bar"), exactly("baz"))
-assert run_parser(foo_bar_or_baz, "foo") == Success("foo")
-assert run_parser(foo_bar_or_baz, "bar") == Success("bar")
-assert run_parser(foo_bar_or_baz, "baz") == Success("baz")
-assert run_parser(foo_bar_or_baz, "qux").failed
-print(run_parser(foo_bar_or_baz, "qux"))
+test(foo_bar_or_baz, "foo", Success("foo"))
+test(foo_bar_or_baz, "bar", Success("bar"))
+test(foo_bar_or_baz, "baz", Success("baz"))
+test(foo_bar_or_baz, "qux", Failure, print_failure=True)
 
 
 @parser_factory
@@ -103,33 +126,32 @@ async def foo_parser():
     return foo
 
 
-assert run_parser(foo_parser(), "<fooooo>").succeeded
-assert run_parser(foo_parser(), "<foxooo>").failed
-print(run_parser(foo_parser(), "<foxooo>"))
-assert run_parser(foo_parser(), "<fooxoo>").failed
-print(run_parser(foo_parser(), "<fooxoo>"))
+test(foo_parser(), "<fooooo>", Success)
+test(foo_parser(), "<fo*ooo>", Failure, print_failure=True)
+test(foo_parser(), "<foo*oo>", Failure, print_failure=True)
 
 
-assert run_parser(py_int(), "-123") == Success(-123)
-assert run_parser(py_int(), "0") == Success(0)
-assert run_parser(py_int(), "-0") == Success(0)
-assert run_parser(py_int(), "-").failed
+test(py_int(), "-123", Success(-123))
+test(py_int(), "0", Success(0))
+test(py_int(), "-0", Success(0))
+test(py_int(), "-", Failure)
 
-assert run_parser(py_float(), "-123.456") == Success(-123.456)
-assert run_parser(py_float(), "-0.456") == Success(-0.456)
-assert run_parser(py_float(), "-.456") == Success(-0.456)
-assert run_parser(py_float(), "-123.") == Success(-123.0)
-assert run_parser(py_float(), "123.456") == Success(123.456)
-assert run_parser(py_float(), "0.456") == Success(0.456)
-assert run_parser(py_float(), ".456") == Success(0.456)
-assert run_parser(py_float(), "123.") == Success(123.0)
-assert run_parser(py_float(), "123.4e5") == Success(123.4e5)
-assert run_parser(py_float(), ".4e5") == Success(0.4e5)
-assert run_parser(py_float(), "1_2_3.4e5") == Success(1_2_3.4e5)
-assert run_parser(py_float(), "1_2_3.4_4e1_1") == Success(1_2_3.4_4e1_1)
-assert run_parser(py_float(), "123").failed
+test(py_float(), "-123.456", Success(-123.456))
+test(py_float(), "-0.456", Success(-0.456))
+test(py_float(), "-.456", Success(-0.456))
+test(py_float(), "-123.", Success(-123.0))
+test(py_float(), "123.456", Success(123.456))
+test(py_float(), "0.456", Success(0.456))
+test(py_float(), ".456", Success(0.456))
+test(py_float(), "123.", Success(123.0))
+test(py_float(), "123.4e5", Success(123.4e5))
+test(py_float(), ".4e5", Success(0.4e5))
+test(py_float(), "1_2_3.4e5", Success(1_2_3.4e5))
+test(py_float(), "1_2_3.4_4e1_1", Success(1_2_3.4_4e1_1))
+test(py_float(), "123", Failure)
 for x in ["inf", "-inf", "Infinity", "-Infinity"]:
-    assert run_parser(py_float(allow_special_values=True), x) == Success(float(x))
+    test(py_float(allow_special_values=True), x, Success(float(x)))
+
 assert math.isnan(
     cast(Success, run_parser(py_float(allow_special_values=True), "nan")).parsed
 )
@@ -138,46 +160,50 @@ assert math.isnan(
 )
 
 
-assert run_parser(optional(exactly("foo")), "foo") == Success("foo")
-assert run_parser(optional(exactly("foo")), "bar") == Success(None, rest="bar")
-assert run_parser(optional(exactly("foo"), default=-1), "bar") == Success(
-    -1, rest="bar"
-)
+test(optional(exactly("foo")), "foo", Success("foo"))
+test(optional(exactly("foo")), "bar", Success(None, rest="bar"))
+test(optional(exactly("foo"), default=-1), "bar", Success(-1, rest="bar"))
 
-assert run_parser(py_int().map(lambda x: x * x), "12") == Success(144)
-assert run_parser(py_int().map(lambda x: x * x), "x").failed
-print(run_parser(py_int().map(lambda x: x * x), "x"))
+test(py_int().map(lambda x: x * x), "12", Success(144))
+test(py_int().map(lambda x: x * x), "x", Failure, print_failure=True)
 
-assert run_parser(recognize(preceded(exactly("#"), py_int())), "#123") == Success(
-    "#123"
-)
+test(recognize(preceded(exactly("#"), py_int())), "#123", Success("#123"))
 
 int_passing_examples = [
-    ("0", 0),
-    ("00000", 00000),
-    ("0_0_0_000_0", 0_0_0_000_0),
-    ("-00000", -00000),
-    ("1", 1),
-    ("1234", 1234),
-    ("123_532_25", 123_532_25),
-    ("-1", -1),
-    ("-0", -0),
-    ("0x2_3Fac_E", 0x2_3FAC_E),
-    ("0o45_6701_20", 0o45_6701_20),
-    ("0b_1001_1", 0b_1001_1),
-    ("-0B_1101", -0b_1101),
-    ("-0O_77171", -0o_77171),
-    ("-0X_DEADBEEF", -0x_DEADBEEF),
+    "0",
+    "00000",
+    "0_0_0_000_0",
+    "-00000",
+    "1",
+    "1234",
+    "123_532_25",
+    "-1",
+    "-0",
+    "0x2_3Fac_E",
+    "0o45_6701_20",
+    "0b_1001_1",
+    "-0B_1101",
+    "-0O_77171",
+    "-0X_DEADBEEF",
 ]
-for example, ans in int_passing_examples:
-    try:
-        assert run_parser(py_int(), example) == Success(ans)
-    except AssertionError:
-        print("Failed for example:", repr(example))
-        print(run_parser(py_int(), example))
-        raise
+for example in int_passing_examples:
+    test(py_int(), example, Success(eval(example)))
 
-print()
-print("--------------------")
-print("✅ All tests passed.")
-print("--------------------")
+
+test(exactly("[") >> py_int() << exactly("]"), "[123]", Success(123))
+print(exactly("[") >> py_int() << exactly("]"))
+print(exactly("fooooo") | exactly("foo") | exactly("f"))
+test(exactly("fooooo") | exactly("foo") | exactly("f"), "fooooo", Success("fooooo"))
+test(exactly("fooooo") | exactly("foo").map(int) | exactly("f"), "f", Success("f"))
+
+
+if errors_so_far == 0:
+    print()
+    print("--------------------")
+    print("✅ All tests passed.")
+    print("--------------------")
+else:
+    print()
+    print("!!!!!!!!!!!!!!!!!!!!")
+    print(f"❌ {errors_so_far} tests failed!")
+    print("!!!!!!!!!!!!!!!!!!!!")
